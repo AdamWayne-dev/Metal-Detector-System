ff4190fea1db04f59f12daf8baab907adf447af4
Adam Wayne <adamwayne.dev@gmail.com>
2023-08-17 20:39:30 +0100

    Added a slow-down effect when holding right click. This will work alonside the scanning mechanic once it's implemented.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public CharacterController controller;
+    
     // Start is called before the first frame update
    
     // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            speed = 1f;
+        }
+        else
+        {
+            speed = 5f;
+        }
+        PlayerMove();
+    }
+
+    void PlayerMove()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
